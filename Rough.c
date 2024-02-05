f16aeb65eff826b7d196da1f8fc0a64a1f69f14c
muzamil majeed <muzamilmajeed34@gmail.com>
2024-02-05 16:25:08 +0530

    first commit

---
+++ Rough.c
@@ -0,0 +1,32 @@
+//1. Variable is the name of a memory location which stores some data
+// Rules:
+// 1 variable are case sensitive
+// 2. Ist character is alphabet or '_'(underscore)
+// 3. No comma/blank space
+// 4. No other symbol other than underscore
+
+
+//2. Constants:-
+//    value that do not change (fixed)
+//     Types :-   Integer  Constants e.g 1, 2, 3, -1, -2 etc
+//                Real Constants   E.g  1.0, 2.0, 3.14 etc
+//                Character Constants   E.g  'a' , 'b' , 'A', '#' etc
+
+// 3. Keywords :-Reserved words that have special meaning to the compiler.
+//       32 Total keywords in total, in C
+//       E.g int, return, long, if etc
+
+/* 4. comments:- That are not part of code  E.g this is double line comment 
+*/
+
+/* 5. Output :to display we use printf("Hello World ")  
+new line :-  printf("kuch bhi\n");
+
+  CASES 
+     1. integers print :- printf("age is %d",age)
+     2. Real no's print :- printf("value of pi is %f",pi)
+     3. Characters print:- printf("star looks like this %c" star)
+
+    6. Input  :- to take from user ir keyboard we use scanf("%d", &age)   here & is addresss
+        
+*/
