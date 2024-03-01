fbd3f019c4d232b86970cd6abed2293d98045d67
Muzamil Majeed <90823440+Muzamil2956@users.noreply.github.com>
2024-03-01 16:14:17 +0530

    Add files via upload

---
+++ CalculateSumfromRange.c
@@ -0,0 +1,9 @@
+// calculate the sum of all nos between 5 and 50 including 5 and 50
+#include<stdio.h>
+int main() {
+    int sum=0;
+    for(int i=5; i<=50; i++) {
+        sum= sum+i;
+    }
+        printf("%d\n", sum);
+    }
